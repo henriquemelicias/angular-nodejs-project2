c19f1f86f00450b968eee540082a7548ae6b9217
LuckyeLies <ezequielbarreira@hotmail.com>
2022-05-05 00:19:05 +0100

    COrreção de erro

--- backend/models/user.schema.js
+++ backend/models/user.schema.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
     {
-        username: { type: String, required: true, minlength: 3, unique: true },
-        password: { type: String, required: true, minLength: 8 }, // is a token
+        username: { type: String, required: true, minlength: 3, unique: true, match: "[a-zA-Z0-9]{ 3 , }" },
+        password: { type: String, required: true, minLength: 8, lowercase: true, uppercase: true, number: true }, // is a token
         roles: [ { type: mongoose.Schema.Types.ObjectId, ref: "Role" } ]
     } );
 
