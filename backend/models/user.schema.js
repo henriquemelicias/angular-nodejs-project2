42ce7525fad44cedcb98c1a42cfd0ab1b066f531
GonsaloDomingues <gonsalo.domingues@gmail.com>
2022-05-06 19:48:18 +0100

    task tem user e users tem tasks

--- backend/models/user.schema.js
+++ backend/models/user.schema.js
@@ -1,5 +1,4 @@
 const mongoose = require( 'mongoose' );
-const taskSchema = require('./task.schema');
 
 const Schema = mongoose.Schema;
 
@@ -8,7 +7,7 @@ const UserSchema = new Schema(
         username: { type: String, required: true, minlength: 3, unique: true },
         password: { type: String, required: true, minLength: 8 }, // is a token
         roles: [ { type: String, required: true } ],
-        tasks: [{ type: taskSchema, required: false }]
+        tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }]
     } );
 
 UserSchema.index( { username: 1 } );
--- frontend/src/app/data/task/schemas/task.schema.ts
+++ frontend/src/app/data/task/schemas/task.schema.ts
@@ -1,7 +1,10 @@
+import { UserSchema } from "@app/data/user/schemas/user.schema";
+
 export interface TaskSchema {
     _id?: string;
     name: string;
     priority: string;
     percentage: number;
     madeByUser: string;
+    users: UserSchema[];
 }
