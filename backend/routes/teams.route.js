6f672a3fae0c027334349f1eff9a25903cac8669
Henrique Lopes <runry.melicias@gmail.com>
2022-05-06 07:45:40 +0100

    add teams done

--- backend/routes/teams.route.js
+++ backend/routes/teams.route.js
@@ -4,8 +4,11 @@ const teamsController = require( '#controllers/teams.controller' );
 const { verifyIfAdmin } = require( "#middlewares/core/verify-admin.middleware" );
 const { verifyToken } = require( "#middlewares/auth/auth.middleware" );
 const { verifyRules } = require( "#middlewares/core/verify-rules.middleware" );
+const { checkDuplicateName } = require( "#middlewares/teams/teams.middleware" );
 
-teamsRouter.post( '/', [verifyToken, verifyIfAdmin, verifyRules( teamsController.getTeamRules() )], teamsController.addTeam );
+teamsRouter.post( '/',
+    [verifyToken, verifyIfAdmin, verifyRules( teamsController.getTeamRules() ), checkDuplicateName ],
+    teamsController.addTeam );
 
 teamsRouter.get( '/:name', teamsController.getTeamByName );
 
